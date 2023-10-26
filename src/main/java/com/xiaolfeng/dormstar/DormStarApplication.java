52277425652e3e119ee29ead6c08843653d7f386
XiaoLFeng <gm@x-lf.cn>
2023-10-26 15:00:07 +0800

    添加Tailwind和构造其他方式

--- src/main/java/com/xiaolfeng/dormstar/DormStarApplication.java
+++ src/main/java/com/xiaolfeng/dormstar/DormStarApplication.java
@@ -3,9 +3,12 @@ package com.xiaolfeng.dormstar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 筱锋xiao_lfeng
+ * @version v1.0.0
+ */
 @SpringBootApplication
 public class DormStarApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DormStarApplication.class, args);
     }
