31a4ef5470f95a3f1cab1dd3f319d40e1fd37f00
XiaoLFeng <gm@x-lf.cn>
2023-11-28 10:02:44 +0800

    Initial commit

---
+++ src/main/kotlin/com/xlf/dromstarkotlin/DromStarKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.xlf.dromstarkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DromStarKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<DromStarKotlinApplication>(*args)
+}
