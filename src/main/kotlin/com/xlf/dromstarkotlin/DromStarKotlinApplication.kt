8ff8fbd317c29dfd646d70cf40b9753ecc8b4f94
XiaoLFeng <gm@x-lf.cn>
2023-11-29 22:12:07 +0800

    SpringBoot配置内容提交

--- src/main/kotlin/com/xlf/dromstarkotlin/DromStarKotlinApplication.kt
+++ src/main/kotlin/com/xlf/dromstarkotlin/DromStarKotlinApplication.kt
@@ -2,8 +2,12 @@ package com.xlf.dromstarkotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.EnableAspectJAutoProxy
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
+@EnableScheduling
 class DromStarKotlinApplication
 
 fun main(args: Array<String>) {
--- src/test/kotlin/com/xlf/dromstarkotlin/DromStarKotlinApplicationTests.kt
+++ src/test/kotlin/com/xlf/dromstarkotlin/DromStarKotlinApplicationTests.kt
@@ -2,12 +2,32 @@ package com.xlf.dromstarkotlin
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.*
 
 @SpringBootTest
 class DromStarKotlinApplicationTests {
-
     @Test
     fun contextLoads() {
+        var token = (Date().time/1000).toString()
+        val random = Random()
+        // 判定随机范围
+        var i: Int
+        var j = 0
+        while (j++ < 3) {
+            val x: Int = if(j == 1 || j == 2) 6 else 10
+            token += "-"
+            i = 0
+            while (i < x) {
+                if (random.nextBoolean()) {
+                    token += random.nextInt(10)
+                } else {
+                    token += random.nextInt(97, 123).toChar()
+                }
+                i++
+            }
+        }
+        println(token)
+        assert(true)
     }
 
 }
